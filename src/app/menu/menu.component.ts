7acb41c1a65c18cc16124d808e94f64faf3a24e5
Emmanuel Salunga <emmanuel.salunga1@aon.com>
2020-07-15 22:56:37 +0800

    fix menu link bug

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WaterService } from '../water.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: WaterService) {}
 
   ngOnInit(): void {
+    if(!this.service.current_user.isLogin)
+      this.service.router.navigateByUrl('/login');
   }
 
 }
